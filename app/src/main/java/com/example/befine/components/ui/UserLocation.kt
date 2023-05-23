7bdefdac366d9e9bd318a8bee0fdeaf60841d09a
Roihan <hroihan6@gmail.com>
2023-05-23 22:58:33 +0700

    Create component UserLocation

---
+++ app/src/main/java/com/example/befine/components/ui/UserLocation.kt
@@ -0,0 +1,26 @@
+package com.example.befine.components.ui
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun UserLocation(location: String) {
+    Row {
+        Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "")
+        Text(
+            location,
+            modifier = Modifier.padding(start = 4.dp),
+            color = Color(0xffaaaaaa),
+            fontWeight = FontWeight.Bold
+        )
+    }
+}

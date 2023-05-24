d0f150fe5a1919f30f47dc552f13e13e06802211
Roihan <hroihan6@gmail.com>
2023-05-24 11:22:10 +0700

    Create components for used in RepairShopItem & NearbyRepairShopItem

--- app/src/main/java/com/example/befine/components/ui/RepairShopName.kt
+++ app/src/main/java/com/example/befine/components/ui/RepairShopName.kt
@@ -1,4 +1,19 @@
 package com.example.befine.components.ui
 
-class RepairShopName {
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun RepairShopName(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = name,
+        fontWeight = FontWeight.Bold,
+        fontSize = 13.sp,
+        modifier = modifier,
+        overflow = TextOverflow.Ellipsis
+    )
 }
--- app/src/main/java/com/example/befine/components/ui/Status.kt
+++ app/src/main/java/com/example/befine/components/ui/Status.kt
@@ -1,4 +1,40 @@
 package com.example.befine.components.ui
 
-class Status {
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.befine.R
+
+object STATUS {
+    const val OPEN = "open"
+    const val CLOSED = "closed"
+}
+
+@Composable
+fun Status(status: String, modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Icon(
+            painter = painterResource(id = R.drawable.baseline_schedule_24),
+            contentDescription = "",
+            modifier = Modifier.width(12.dp),
+            tint = if (status == STATUS.CLOSED) Color.Red else Color.Green
+        )
+        Text(
+            text = if (status == STATUS.OPEN) "Open" else "Closed",
+            fontSize = 9.sp,
+            modifier = Modifier.padding(start = 2.dp)
+        )
+    }
 }

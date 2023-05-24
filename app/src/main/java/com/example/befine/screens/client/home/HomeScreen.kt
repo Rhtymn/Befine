6fa453e47cf65352b3e22793c53091a316baa31d
Roihan <hroihan6@gmail.com>
2023-05-24 20:56:17 +0700

    Update HomeScreen UI for client and create HomeViewModel

--- app/src/main/java/com/example/befine/screens/client/home/HomeScreen.kt
+++ app/src/main/java/com/example/befine/screens/client/home/HomeScreen.kt
@@ -1,6 +1,9 @@
 package com.example.befine.screens.client
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.outlined.AccountCircle
@@ -9,14 +12,52 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.befine.components.ui.NearbyRepairShopItem
+import com.example.befine.components.ui.RepairShopItem
 import com.example.befine.components.ui.UserLocation
+import com.example.befine.data.RepairShopData
+import com.example.befine.model.RepairShop
 import com.example.befine.ui.theme.BefineTheme
 import com.example.befine.utils.Screen
 
 @Composable
 fun HomeScreen() {
+}
+
+@Composable
+fun HeaderText(value: String, modifier: Modifier = Modifier) {
+    Text(
+        value,
+        fontSize = 16.sp,
+        fontWeight = FontWeight.Bold,
+        modifier = modifier.padding(bottom = 12.dp)
+    )
+}
+
+@Composable
+fun NearbyShopList(data: List<RepairShop>) {
+    LazyRow(modifier = Modifier.padding(bottom = 24.dp)) {
+        items(data, key = { it.id }) {
+            NearbyRepairShopItem(
+                name = it.name,
+                distance = "0.7 km",
+                status = it.status
+            )
+        }
+    }
+}
+
+@Composable
+fun OthersList(data: List<RepairShop>) {
+    LazyColumn() {
+        items(data, key = { it.id }) {
+            RepairShopItem(name = it.name, status = it.status, address = it.address)
+        }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -77,7 +118,14 @@ fun HomeScreenPreview() {
                             vertical = Screen.paddingVertical
                         )
                 ) {
-                    UserLocation(location = "User Location")
+                    UserLocation(
+                        location = "User Location",
+                        modifier = Modifier.padding(bottom = 8.dp)
+                    )
+                    HeaderText(value = "Nearby Repair Shops")
+                    NearbyShopList(data = RepairShopData.repairShop)
+                    HeaderText(value = "Others")
+                    OthersList(data = RepairShopData.repairShop)
                 }
             }
         }
--- app/src/main/java/com/example/befine/screens/client/home/HomeViewModel.kt
+++ app/src/main/java/com/example/befine/screens/client/home/HomeViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.befine.screens.client.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+import com.example.befine.model.RepairShop
+import com.example.befine.repository.RepairShopRepository
+
+class HomeViewModel(private val repository: RepairShopRepository): ViewModel() {
+    fun getAllRepairShops(): List<RepairShop> {
+        return repository.getRepairShops()
+    }
 }

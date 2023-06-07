1c492a675f9c2ccf41e61a145f2df8fcc1377c13
Roihan <hroihan6@gmail.com>
2023-06-08 00:46:46 +0700

    Change some UI in RepairShopDetailScreen

--- app/src/main/java/com/example/befine/screens/client/details/RepairShopDetailsScreen.kt
+++ app/src/main/java/com/example/befine/screens/client/details/RepairShopDetailsScreen.kt
@@ -1,24 +1,31 @@
 package com.example.befine.screens.client.details
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Button
 import androidx.compose.material.Icon
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Email
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.zIndex
 import com.example.befine.R
 import com.example.befine.components.ui.RepairShopName
 import com.example.befine.ui.theme.BefineTheme
@@ -108,27 +115,52 @@ fun RepairShopDetailsScreen(
                     }
                 }
                 Divider(modifier = Modifier.padding(vertical = 10.dp))
-                Button(onClick = { /*TODO*/ }, modifier = Modifier.padding(top = 6.dp)) {
+                Button(
+                    onClick = { /*TODO*/ },
+                    modifier = Modifier.padding(top = 6.dp),
+                    colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+                ) {
                     Icon(
                         imageVector = Icons.Filled.Email,
                         contentDescription = "",
-                        modifier = Modifier.padding(end = 4.dp)
+                        modifier = Modifier.padding(end = 4.dp),
+                        tint = Color.White
                     )
-                    Text("Send Message")
+                    Text("Send Message", color = Color.White)
                 }
             }
         }) {
-        GoogleMap(
-            modifier = Modifier
-                .fillMaxSize(),
-            cameraPositionState = cameraPositionState,
-            onMapClick = { _ -> scope.launch { scaffoldState.bottomSheetState.partialExpand() } },
-        ) {
-            Marker(
-                state = MarkerState(position = singapore),
-                title = "Singapore",
-                snippet = "Marker in Singapore"
-            )
+        Box() {
+            Column(
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(start = 15.dp, top = 15.dp)
+                    .zIndex(10f)
+                    .clip(CircleShape)
+                    .background(color = MaterialTheme.colorScheme.primary)
+
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.ArrowBack,
+                    contentDescription = "",
+                    tint = Color.White,
+                    modifier = Modifier
+                        .size(35.dp)
+                )
+            }
+
+            GoogleMap(
+                modifier = Modifier
+                    .fillMaxSize(),
+                cameraPositionState = cameraPositionState,
+                onMapClick = { _ -> scope.launch { scaffoldState.bottomSheetState.partialExpand() } },
+            ) {
+                Marker(
+                    state = MarkerState(position = singapore),
+                    title = "Singapore",
+                    snippet = "Marker in Singapore"
+                )
+            }
         }
     }
 }

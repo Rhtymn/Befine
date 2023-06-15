54f441a98e48de45fda6c21bbbcbe5d95ff41b0e
Roihan <hroihan6@gmail.com>
2023-06-15 20:21:05 +0700

    Show circular progress when try to convert latitude & longitude to user address location

--- app/src/main/java/com/example/befine/components/ui/UserLocation.kt
+++ app/src/main/java/com/example/befine/components/ui/UserLocation.kt
@@ -3,8 +3,10 @@ package com.example.befine.components.ui
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.LocationOn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,14 +22,24 @@ import androidx.compose.ui.unit.sp
 fun UserLocation(location: String, modifier: Modifier = Modifier) {
     Row(modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
         Icon(imageVector = Icons.Filled.LocationOn, contentDescription = "")
-        Text(
-            location,
-            modifier = Modifier.padding(start = 4.dp),
-            color = Color(0xffaaaaaa),
-            fontWeight = FontWeight.Bold,
-            fontSize = 12.sp,
-            maxLines = 1,
-            overflow = TextOverflow.Ellipsis
-        )
+        if (location == "User location") {
+            CircularProgressIndicator(
+                Modifier
+                    .padding(start = 5.dp)
+                    .size(16.dp),
+                strokeWidth = 2.dp,
+                color = Color.Gray
+            )
+        } else {
+            Text(
+                location,
+                modifier = Modifier.padding(start = 4.dp),
+                color = Color(0xffaaaaaa),
+                fontWeight = FontWeight.Bold,
+                fontSize = 12.sp,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
+        }
     }
 }
